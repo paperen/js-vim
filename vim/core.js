2530581ba2451968c88853cb8c10aefa63287520
paperen <paperen@gmail.com>
2013-05-23 13:46:45 +0800

    Signed-off-by: paperen <paperen@gmail.com>

---
+++ vim/core.js
@@ -0,0 +1,21 @@
+/**
+ * VIM js
+ * @author paperen<paperen@gmail.com>
+ * @link http://iamlze.cn
+ */
+
+(function($){
+    $.fn.vim = function(options) {
+        var defaults = {
+        },
+        keymap = {
+
+        },
+        settings = $.extend({}, defaults, options);
+
+        // keydown
+        $(this).keydown(function(e){
+        });
+
+    }
+})(jQuery);
