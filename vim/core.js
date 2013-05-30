37e416ca76e28b96ae3f52ae76d9f49e471a2b2f
paperen <paperen@gmail.com>
2013-05-30 18:32:02 +0800

    Signed-off-by: paperen <paperen@gmail.com>

--- vim/core.js
+++ vim/core.js
@@ -7,7 +7,8 @@
 (function($){
     $.fn.vim = function(options) {
         var defaults = {
-            'debug' : true
+            'debug' : true,
+            'scrollpx' : 50
         },
         // 键值映射
         keyMap = {
@@ -25,17 +26,40 @@
             'k' : 75,
             'd' : 68,
             'u' : 85,
-            'r' : 82
+            'r' : 82,
+            'ESC' : 27,
+            '0' : 48,
+            '9' : 57
         },
         // 命令组合
         commandArr = [
             'y+y',
-            'r'
+            'r',
+            'j',
+            'k',
+            'd',
+            'u',
+            'gg',
+            'SHIFTg',
+            'SHIFTh',
+            'SHIFTl',
+            'f',
+            'F'
         ],
         // 命令解析
         commandTxt = {
             'yy' : '复制当前标签页链接到剪切板',
-            'r' : '重新载入当前页面'
+            'r' : '重新载入当前页面',
+            'j' : '向下滚动',
+            'k' : '向上滚动',
+            'd' : '向下滚动半页',
+            'u' : '向上滚动半页',
+            'gg' : '滚动到页面顶部',
+            'SHIFTg' : '滚动到页面底部',
+            'SHIFTh' : '后退',
+            'SHIFTl' : '前进',
+            'f' : '打开当前页面上某个链接',
+            'F' : '在新标签页中打开当前页面上某个链接'
         },
         // 按键堆栈
         keyStack = [],
@@ -122,5 +146,98 @@
         function r() {
             window.location.reload();
         }
+
+        function j() {
+            $('body').scrollTop( $('body').scrollTop() + settings.scrollpx );
+        }
+
+        function k() {
+            $('body').scrollTop( $('body').scrollTop() - settings.scrollpx );
+        }
+
+        function u() {
+            var offset = $(window).height() / 2;
+            $('body').scrollTop( $('body').scrollTop() - offset );
+        }
+
+        function d() {
+            var offset = $(window).height() / 2;
+            $('body').scrollTop( $('body').scrollTop() + offset );
+        }
+
+        function gg() {
+            $('body').scrollTop(0);
+        }
+
+        function SHIFTg() {
+            var offset = $('body').height();
+            $('body').scrollTop(offset);
+        }
+
+        function SHIFTh() {
+            window.history.go(-1);
+        }
+
+        function SHIFTl() {
+            window.history.go(1);
+        }
+
+        function f() {
+            if ( $('.js-vim-hint').length > 0 ) return;
+            var hint = $('<span>').css({
+                'color' : '#000',
+                'font-weight' : 'bold',
+                'font-size' : '12px',
+                'font-family' : 'Vendana',
+                'padding' : '2px',
+                'background' : '#FFD76E',
+                'position' : 'absolute',
+                'border-radius' : '4px',
+                'opacity' : '0.8',
+            }).attr('class', 'js-vim-hint');
+            var index = 10;
+            $('a').each(function(){
+                var offset = $(this).offset();
+                $(hint).clone()
+                .html(index)
+                .offset({top: offset.top, left: offset.left})
+                .data('target', $(this))
+                .appendTo('body');
+                index++;
+            });
+            // bind
+            $(document).bind('keydown', fKeydown);
+        }
+
+        //only for f
+        var fkeyStack = [];
+        function fKeydown(e) {
+            if ( e.keyCode == keyMap.ESC ) {
+                // over find mode
+                $(document).unbind('keydown', fKeydown);
+                // remove all hint
+                $('.js-vim-hint').remove();
+                //clear fkeyStack
+                fkeyStack = [];
+                return;
+            } else if( e.keyCode >= keyMap['0']  && e.keyCode <= keyMap['9'] ) {
+                // record number
+                fkeyStack.push( e.keyCode - keyMap['0'] );
+                var currentIndex = parseInt( fkeyStack.join('') );
+                $('.js-vim-hint').each(function(){
+                    if ( currentIndex == parseInt( $(this).html() ) ) {
+                        //trigger click
+                        var evt = document.createEvent("MouseEvents");
+                        evt.initEvent("click", true, true);
+                        $(this).data('target').get(0).dispatchEvent(evt);
+                    } else {
+                        //filter
+                        var regx = new RegExp('^' + currentIndex);
+                        if ( !regx.test( $(this).html() ) ) $(this).remove();
+                    }
+                });
+            }
+        }
+
     }
 })(jQuery);
